298d58b3430d81f3dab182c03b4d24dd7ae241c3
JacintaNgwu <jacintangwu9900@gmail.com>
2022-12-14 14:33:14 +0530

    Add task.js file

---
+++ task.js
@@ -0,0 +1,41 @@
+// task one
+const stringLength = (string) => {
+    const lengthOfString= string.length;
+    if (lengthOfString > 0 && lengthOfString < 10) {
+      return lengthOfString;
+    }
+    throw new Error('String length should not exceed 10 characters');
+  };
+
+// task two
+
+const reverseString = string => {
+    let myString = string.split("").reverse().join('');
+    return myString
+}
+
+// task three
+class Calculator {
+    static add(a, b) {
+     return a + b
+    }
+    static subtract(a, b) {
+      return a - b
+    }
+
+    static divide(a, b){
+       return a / b;
+    }
+
+    static multiply(a, b) {
+        return a * b
+    }
+}
+
+// task four
+const capitalize = string => {
+    var capitalizedString = string.charAt(0).toUpperCase() + string.slice(1);
+   return (capitalizedString);
+}
+
+module.exports = { stringLength, reverseString, Calculator, capitalize };
